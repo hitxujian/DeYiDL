deee7825b194df53627166f1b1336a81d894c184
m516825 <b01902078@ntu.edu.tw>
2015-10-20 01:20:34 +0800

    SUCCESS!!!

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -90,26 +90,29 @@ if __name__ == "__main__" :
 		yHatAll.append(mapping[label[1]])
 
 	def MyUpdate(paramaters, gradients):
-		mu = numpy.float32(0.1)
+		mu = numpy.float32(0.05)
 		paramaters_update = \
 		[(p, p - mu * g) for p, g in izip(paramaters, gradients) ]
 		return paramaters_update
 	
 	x = T.matrix(dtype='float32')
-	w = theano.shared(numpy.asmatrix(numpy.ones((128,39),dtype='float32')))
-	b = theano.shared(numpy.ones((128),dtype='float32'))
+	w = theano.shared(numpy.random.randn(128,39).astype(dtype='float32'))
+	b1 = theano.shared(numpy.ones((128),dtype='float32'))
+	b2 = theano.shared(numpy.ones((48),dtype='float32'))
 
 	#print x.type
 
-	wy = theano.shared(numpy.asmatrix(numpy.ones((48,128),dtype='float32')))
+	wy = theano.shared(numpy.random.randn(48,128).astype(dtype='float32'))
 	
-	a1 = 1/(1+T.exp(-1*(T.dot(w,x) + b.dimshuffle(0,'x'))))
-	y = 1/(1+T.exp(-1*(T.dot(wy, a1))))
+	z1 = T.dot(w,x) + b1.dimshuffle(0,'x')
+	a1 = 1/(1+T.exp(-z1))
+	z2 = T.dot(wy, a1) + b2.dimshuffle(0,'x')
+	y = 1/(1+T.exp(-z2))
 	
 	y_hat = T.matrix(dtype='float32')
 	cost = T.sum((y-y_hat)**2)
 
-	gradients = T.grad(cost, [w, b, wy])
+	gradients = T.grad(cost, [w, b1, wy])
 
 	neuron1 = theano.function(inputs=[x],outputs=a1)
 	neuron2 = theano.function(inputs=[a1],outputs=y)
@@ -117,22 +120,25 @@ if __name__ == "__main__" :
 	#mu = numpy.float32(0.1)
 	#print mu
 	#print type(mu)
-	train = theano.function(inputs=[x, y_hat], updates=MyUpdate([w, b, wy], gradients), outputs=cost)
+	train = theano.function(inputs=[x, y_hat], updates=MyUpdate([w, b1, wy], gradients), outputs=cost)
 	test = theano.function(inputs=[x], outputs=y)
-	x = numpy.matrix([[1,1],[-1,1],[1,1]], dtype='float32')#[[1, -1, 1],[1, 1, 1]]
+	#x = numpy.matrix([[1,1],[-1,1],[1,1]], dtype='float32')#[[1, -1, 1],[1, 1, 1]]
 	#x = numpy.array(x).astype(dtype='float32')
 	#print x.type.dtype
-	y_hat = numpy.matrix([[0,1],[1,0],[0,0]], dtype='float32')#[[0, 1, 0],[1, 0, 0]]
+	#y_hat = numpy.matrix([[0,1],[1,0],[0,0]], dtype='float32')#[[0, 1, 0],[1, 0, 0]]
 	#x = numpy.array(y_hat).astype(dtype='float32')
 
-	for t in range(1000):
+	for t in range(10000):
 		cost = 0
 		dataSize = len(xAll[0])
 		xBatch, yHatBatch = mkBatch(xAll, yHatAll, dataSize, 6)
 		for i in range(10):
-			cost+=train(xBatch[i],yHatBatch[i])
+			cost += train(xBatch[i],yHatBatch[i])
 		cost/=10
 		print cost
+
+	print test(xBatch[0])
+	print yHatBatch[0]
 	
 	# s = time.time()
 	# for i in range(1000):
