ea0fc3ee8e72b5ad02b76f69c77d8f4d1b955585
m516825 <b01902078@ntu.edu.tw>
2015-10-20 00:01:14 +0800

    first update

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -8,23 +8,23 @@ import time
 if __name__ == "__main__" :
 
 	def MyUpdate(paramaters, gradients):
-		mu = 0.1
+		mu = numpy.float32(0.1)
 		paramaters_update = \
 		[(p, p - mu * g) for p, g in izip(paramaters, gradients) ]
 		return paramaters_update
 	
-	x = T.matrix()
-	w = theano.shared(numpy.asmatrix(numpy.ones((4,3))))
-	b = theano.shared(numpy.ones(4))
+	x = T.matrix(dtype='float32')
+	w = theano.shared(numpy.asmatrix(numpy.ones((4,3),dtype='float32')))
+	b = theano.shared(numpy.ones((4),dtype='float32'))
 
-	print x.type
+	#print x.type
 
-	wy = theano.shared(numpy.asmatrix(numpy.ones((3,4))))
+	wy = theano.shared(numpy.asmatrix(numpy.ones((3,4),dtype='float32')))
 	
 	a1 = 1/(1+T.exp(-1*(T.dot(w,x) + b.dimshuffle(0,'x'))))
 	y = 1/(1+T.exp(-1*(T.dot(wy, a1))))
 	
-	y_hat = T.matrix()
+	y_hat = T.matrix(dtype='float32')
 	cost = T.sum((y-y_hat)**2)
 
 	gradients = T.grad(cost, [w, b, wy])
@@ -32,6 +32,9 @@ if __name__ == "__main__" :
 	neuron1 = theano.function(inputs=[x],outputs=a1)
 	neuron2 = theano.function(inputs=[a1],outputs=y)
 
+	#mu = numpy.float32(0.1)
+	#print mu
+	#print type(mu)
 	train = theano.function(inputs=[x, y_hat], updates=MyUpdate([w, b, wy], gradients), outputs=cost)
 	test = theano.function(inputs=[x], outputs=y)
 	x = numpy.matrix([[1,1],[-1,1],[1,1]], dtype='float32')#[[1, -1, 1],[1, 1, 1]]
@@ -42,17 +45,16 @@ if __name__ == "__main__" :
 
 	
 	s = time.time()
-	
 	for i in range(1000):
-		print "--------"+str(i+1)+"--------"
-		print train(x, y_hat)
-		print test(x)
-		print w.get_value(), b.get_value()
-		print wy.get_value()
+		#print "--------"+str(i+1)+"--------"
+		train(x, y_hat)
+		test(x)
+		# print w.get_value(), b.get_value()
+		# print wy.get_value()
 	f = time.time()
 
 	print f-s
-
+	
 	
 	"""
 	gradient = theano.function(inputs=[x, y_hat], outputs=[dw, db])
